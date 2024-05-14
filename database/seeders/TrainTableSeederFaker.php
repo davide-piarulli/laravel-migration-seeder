8403bd009c1f243e1016d54eab3ac3d939df18ef
davide-piarulli <davide.piarulli.1@gmail.com>
2024-05-14 15:30:17 +0100

    [add] database seeded 25 elements

--- database/seeders/TrainTableSeederFaker.php
+++ database/seeders/TrainTableSeederFaker.php
@@ -4,8 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Train;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class TrainTableSeederFaker extends Seeder
 {
@@ -15,9 +16,20 @@ class TrainTableSeederFaker extends Seeder
     public function run(Faker $faker): void
     {
         //passo nuova istanza del faker come parametro del metodo run
-        // for($i = 0; $i < 100; $i++)
-        // {
-
-        // }
+        for ($i = 0; $i < 25; $i++) {
+            $new_train = new Train();
+            $new_train->travel_company = $faker->words(1, true);
+            $new_train->departure_station = $faker->city();
+            $new_train->arrival_station = $faker->city();
+            $new_train->departure_time = $faker->time();
+            $new_train->arrival_time = $faker->time();
+            $new_train->train_number = $faker->numerify('RE-####');
+            $new_train->wagon_number = $faker->randomDigitNot(0);
+            $new_train->in_time = $faker->boolean();
+            $new_train->minutes_late = $faker->randomDigit();
+            $new_train->is_cancelled = $faker->boolean();
+            // effettuo insert nel db
+            $new_train->save();
+        }
     }
 }
--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -1,5 +1,24 @@
 @extends('layout.main')
 
 @section('content')
-  <h1>Tabellone Treni</h1>
+  <h1>Tabellone Partenze</h1>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">N. Treno</th>
+        <th scope="col">Destinazione</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+        
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+    </tbody>
+  </table>
 @endsection
